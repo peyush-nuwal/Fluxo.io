f46a81b745299fab577b4254808fc8c86c5853e6
Peyush Nuwal <piyushnawal19@gmail.com>
2025-10-01 23:02:58 +0530

    feat(auth-service): implemented update password service

--- apps/auth-service/src/app.js
+++ apps/auth-service/src/app.js
@@ -11,11 +11,16 @@ import securityMiddleware from "./middleware/security.middleware.js";
 const app = express();
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
+app.use(
+  cors({
+    origin: true, // Allow all origins for development
+    credentials: true, // Allow cookies
+  }),
+);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
+app.use(helmet());
 
 // HTTP request logging with Winston + Morgan
 app.use(
