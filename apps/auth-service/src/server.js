9b3ea220e50e302b2113eb6a2802234003aeeaa1
peyush nuwal <piyushnawal19@gmail.com>
2025-10-19 22:00:47 +0530

    Delete apps/auth-service directory

--- apps/auth-service/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-
-const PORT = process.env.PORT || 4001;
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`auth service is running on http://localhost:${PORT}`);
-});
--- apps/auth-service/src/utils/format.js
+++
@@ -1,8 +0,0 @@
-export const formatValidationsError = (errors) => {
-  if (!errors || !errors.issues) return "Validations failed";
-
-  if (Array.isArray(errors.issues))
-    return errors.issues.map((i) => i.message).join(", ");
-
-  return JSON.stringify(errors);
-};
