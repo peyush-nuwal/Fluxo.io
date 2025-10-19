5e1fe8c0162c055033d713e5b5971a9a88f801fd
peyush nuwal <piyushnawal19@gmail.com>
2025-10-19 22:00:47 +0530

    Delete apps/auth-service directory

--- apps/auth-service/src/config/database.js
+++
@@ -1,9 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
-
-const sql = neon(process.env.DATABASE_URL);
-
-const db = drizzle(sql);
-
-export { sql, db };
