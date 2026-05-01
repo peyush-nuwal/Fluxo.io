e6d4ee6b583bc5d90575df5bff0a17abb5ac790f
Peyush Nuwal <piyushnawal19@gmail.com>
2026-05-02 00:54:25 +0530

    chore: update feature grid to bento grid style

--- apps/landing/components/Features.tsx
+++ apps/landing/components/Features.tsx
@@ -1,7 +1,7 @@
 const Features = () => {
   return (
-    <div className="min-h-screen bg-green-200  ">
-      <div className="flex flex-col justify-center items-center py-10">
+    <div className="min-h-screen bg-green-200 py-10 ">
+      <div className="flex flex-col justify-center items-center ">
         <p className="eyebrow">section</p>
         <h1 className="heading">dfsdfsa</h1>
         <p className="description">
@@ -9,13 +9,31 @@ const Features = () => {
           friction.
         </p>
       </div>
-      <div className="grid grid-cols-3 md:grid-cols-5 px-5 max-w-7xl mx-auto">
-        <div className="col-span-1 md:col-span-2 bg-red-500">g</div>
-        <div className="col-span-2 md:col-span-2 bg-blue-500">g</div>
-        <div className="col-span-2 md:col-span-2 bg-green-500">h</div>
-        <div className="col-span-1 md:col-span-2 bg-blue-500">d</div>
-        <div className="col-span-1 md:col-span-2 bg-green-500">d</div>
-        <div className="col-span-2 md:col-span-2 bg-red-500">d</div>
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[200px]">
+          {/* BIG LEFT */}
+          <div className="lg:col-span-1 lg:row-span-2 rounded-2xl bg-purple-300 p-6">
+            Customization
+          </div>
+
+          {/* TOP RIGHT WIDE */}
+          <div className="lg:col-span-2 rounded-2xl bg-pink-300 p-6">
+            Scheduling
+          </div>
+
+          {/* MIDDLE RIGHT SMALL */}
+          <div className="rounded-2xl bg-green-300 p-6">Wallet</div>
+
+          <div className="rounded-2xl bg-yellow-300 p-6">Inbox</div>
+
+          {/* BOTTOM LEFT WIDE */}
+          <div className="lg:col-span-2 rounded-2xl bg-orange-300 p-6">
+            Send Gifts
+          </div>
+
+          {/* BOTTOM RIGHT SMALL */}
+          <div className="rounded-2xl bg-blue-300 p-6">Reminders</div>
+        </div>
       </div>
     </div>
   );
